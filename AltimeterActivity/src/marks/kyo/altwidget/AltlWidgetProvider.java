66ca9f88d06d563f70338019ba02509575f68f7c
mkahrl <mark@kahrlconsulting.com>
2023-05-05 22:21:28 -0700

    Update AltlWidgetProvider.java

--- AltimeterActivity/src/marks/kyo/altwidget/AltlWidgetProvider.java
+++ AltimeterActivity/src/marks/kyo/altwidget/AltlWidgetProvider.java
@@ -11,16 +11,16 @@ import android.widget.RemoteViews;
 
 public class AltlWidgetProvider extends AppWidgetProvider
 {
-		private static String CURRENT_ALT;
-		private static String CURRENT_PRESS;
-		private static String PRESS_UNITS;
-		private static String ALT_UNITS;
-		private final static String TENS_FORMAT = "%.1f";
-		private final static String ZERO_FORMAT = "%.0f";
-		private static String ft;
-		private static String mb;
-		private String p_lbl;
-		String alt_lbl;
+	private static String CURRENT_ALT;
+	private static String CURRENT_PRESS;
+	private static String PRESS_UNITS;
+	private static String ALT_UNITS;
+	private final static String TENS_FORMAT = "%.1f";
+	private final static String ZERO_FORMAT = "%.0f";
+	private static String ft;
+	private static String mb;
+	private String p_lbl;
+	String alt_lbl;
         float press=1.0f;
         float alt=1.0f;
        
@@ -31,18 +31,18 @@ public class AltlWidgetProvider extends AppWidgetProvider
             UnitConvertor convertor = new UnitConvertor(context);
             String alt_units=PreferenceManager.getDefaultSharedPreferences(context).getString(ALT_UNITS, ft);
             String p_units=PreferenceManager.getDefaultSharedPreferences(context).getString(PRESS_UNITS, mb);
-         	alt = PreferenceManager.getDefaultSharedPreferences(context).getFloat(CURRENT_ALT, 0.0f);
-         	alt = convertor.convertAltitude(alt, alt_units);
+            alt = PreferenceManager.getDefaultSharedPreferences(context).getFloat(CURRENT_ALT, 0.0f);
+            alt = convertor.convertAltitude(alt, alt_units);
          	
-         	press = PreferenceManager.getDefaultSharedPreferences(context).getFloat(CURRENT_PRESS, 0.0f);
-         	press = convertor.convertPressure(press, p_units);
+            press = PreferenceManager.getDefaultSharedPreferences(context).getFloat(CURRENT_PRESS, 0.0f);
+            press = convertor.convertPressure(press, p_units);
          	
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.altwidget);
             String p = String.format(TENS_FORMAT, press);
             String a = String.format(ZERO_FORMAT, alt);
         
-        	views.setTextViewText(R.id.alt_label, alt_units);
-        	views.setTextViewText(R.id.press_label, p_units);
+            views.setTextViewText(R.id.alt_label, alt_units);
+            views.setTextViewText(R.id.press_label, p_units);
         	
             views.setTextViewText(R.id.alt, a);
             views.setTextViewText(R.id.press, p);
@@ -55,18 +55,18 @@ public class AltlWidgetProvider extends AppWidgetProvider
             {
                 appWidgetManager.updateAppWidget(appWidgetIds[i], views);
             }
-			super.onUpdate(context, appWidgetManager, appWidgetIds);
+	    super.onUpdate(context, appWidgetManager, appWidgetIds);
 	}
 	
 	private void getStrings(Context ctx)
-    {
-    	alt_lbl=ctx.getResources().getString(R.string.alt_label);
-    	p_lbl=ctx.getResources().getString(R.string.pressure_label);
-  		ft = ctx.getResources().getString(R.string.feet);
-  		mb = ctx.getResources().getString(R.string.millibar);
-  		ALT_UNITS = ctx.getResources().getString(R.string.alt_units);
-  		PRESS_UNITS = ctx.getResources().getString(R.string.press_units);
-  		CURRENT_ALT = ctx.getResources().getString(R.string.current_alt);
-  		CURRENT_PRESS = ctx.getResources().getString(R.string.current_press);
-    }
+        {
+    	     alt_lbl=ctx.getResources().getString(R.string.alt_label);
+    	     p_lbl=ctx.getResources().getString(R.string.pressure_label);
+  	     ft = ctx.getResources().getString(R.string.feet);
+  	     mb = ctx.getResources().getString(R.string.millibar);
+  	     ALT_UNITS = ctx.getResources().getString(R.string.alt_units);
+  	     PRESS_UNITS = ctx.getResources().getString(R.string.press_units);
+  	     CURRENT_ALT = ctx.getResources().getString(R.string.current_alt);
+  	     CURRENT_PRESS = ctx.getResources().getString(R.string.current_press);
+      }
 }
